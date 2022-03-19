8fd6cb436c7dd47601d8a3c702bc3f3b039be64e
drigoalexander <drigosihombinga@gmail.com>
2022-03-19 16:13:11 +0700

    section 12 - Vue CLI

---
+++ 12_VueCLI/praktikum/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+
+  <div class="container">
+    <img src="./assets/logo.png" alt="" width="10%">
+  <h1>Todo List</h1><br>
+  <TodoItem/>
+  </div>
+  
+  </div>
+</template>
+
+<script>
+// import tugas from './'
+import TodoItem from './components/TodoItem.vue'
+export default {
+  name: 'App',
+  components: {
+    TodoItem,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  width: 100vw;
+  height: 50vw;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+}
+.container{
+  width: 50%;
+  margin: auto;
+}
+</style>
---
+++ 12_VueCLI/praktikum/src/components/TodoItem.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+<ul>
+            <li type="1" v-for="list in todo" :key="list">{{ list }} </li>
+        </ul>
+        <input type="text" v-model="value">
+        <button v-on:click="addTodo">add</button>
+        <p v-if="todo.length >= 4">Hebat!</p>
+</div>
+        
+       
+</template>
+
+    <script>
+    export default{
+    name: 'TodoItem',
+    data() {
+            return {
+              todo: [],
+              value: ""
+            }
+          },
+          methods: {
+              addTodo: function(){
+                  if(this.value === '' || this.value === null) alert("Fill the box")
+                  else{
+                    this.todo.push(this.value)
+                    this.value = ""       
+                  }
+              }
+          }
+    }
+      </script>
+ 
+    <style scoped>
+
+
+    input:focus{
+    outline: none !important;
+        border:solid #41B883;
+        transition: border 1s;
+        }
+    button{
+    background-color: #41B883;
+    border: none;
+    width: auto;
+    padding: 5px 10px;
+    border-radius: 5px;
+    margin-left: 10px;}
+
+    button:hover{
+    background-color: #34495E;
+    color: white;
+   }
+    </style>
+    
